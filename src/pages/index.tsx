d5d58e013d3345735e5a23bc522b6747bab3dadb
iammarkps <markkung08@gmail.com>
2020-07-22 22:17:58 +0700

    :sparkles: Add title

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,8 +2,10 @@ import React from 'react'
 
 export default () => (
   <div className="min-h-screen flex flex-col items-center justify-center">
-    <div className="font-display text-6xl font-bold">
-      Next.JS + Tailwind CSS + TypeScript
+    <div className="max-w-3xl font-display">
+      <div className="shadow-md p-8 bg-white rounded-md">
+        <h1 className="text-lg font-bold">พิมพ์ใบขออนุญาตไปทำกิจกรรม</h1>
+      </div>
     </div>
   </div>
 )
